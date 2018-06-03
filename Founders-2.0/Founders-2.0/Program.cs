4d4a8f71f1e690fadc27da0ef38de6bdfdd0811c
Navraj Singh <navrajsingh@Navrajs-iMac.local>
2018-06-03 14:44:11 +0530

    Added Standard Core

---
+++ Founders-2.0/Founders-2.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CloudCoinCore;
+using CloudCoinCoreDirectory;
+
+namespace Founders_2._0
+{
+    class Program
+    {
+        public static RAIDA raida= RAIDA.GetInstance();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
